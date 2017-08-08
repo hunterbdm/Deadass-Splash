b2ffe959b876ed38a16aeb18aad164056ace1f4a
Christian Flores <christian19flores@gmail.com>
2017-08-07 19:05:00 -0500

    working donate

--- build/scripts/app.js
+++ build/scripts/app.js
@@ -570,6 +570,11 @@ function copyToClipboard(text) {
           var window = BrowserWindow.getFocusedWindow();
 					window.close();
         }); 
+
+        document.getElementById("donate").addEventListener("click", function(e){
+          //var window = BrowserWindow.getFocusedWindow();
+          require('electron').shell.openExternal('https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=danthemangall@gmail.com&lc=US&item_name=Deadass-Splash&no_note=0&currency_code=USD&bn=PP-DonationsBF:btn_donate_LG.gif:NonHostedGuest&submit.x=54&submit.y=12')
+        })
       }; 
       
       document.onreadystatechange = function () {
